ad9e20a47254dc79efbb9bdb5560081e9f1a3c41
Shashwat Kashyap <shashwat.kashyap@icloud.com>
2024-01-06 21:32:32 +0530

    Created New Multiplatform Target On Mac And iPhone

---
+++ RunOnMacAndIphone/RunOnMacAndIphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunOnMacAndIphoneApp.swift
+//  RunOnMacAndIphone
+//
+//  Created by Shashwat Kashyap on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct RunOnMacAndIphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
